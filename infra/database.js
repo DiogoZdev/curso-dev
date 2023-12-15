30ee4c436a4005c132ad72144361424b6b534ae2
Diogo <diogo.lara.dev@gmail.com>
2023-12-15 08:59:48 -0300

    chore: `database.js` created
    `database.js` created

---
+++ infra/database.js
@@ -0,0 +1,21 @@
+import { Client } from "pg";
+
+const query = async (queryObject) => {
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+  await client.connect();
+
+  const res = await client.query(queryObject);
+  await client.end();
+
+  return res;
+};
+
+export default {
+  query: query
+};
