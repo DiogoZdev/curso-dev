d6042dd80343d7259b4f15088207a9d1cf7de05b
Diogo <diogo.lara.dev@gmail.com>
2023-12-30 10:58:00 -0300

    refactor: moving `compose` to project root
    moving `compose` to project root

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,5 +1,13 @@
+import database from "../../../../infra/database";
+
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function status(req: NextApiRequest, res: NextApiResponse) {
+export default async function status(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+
   return res.status(200).json({ status: "ok" });
 }
