edaeb5cc377d17e4c2f700a3b9bd5be02c4f92e7
Diogo <diogo.lara.dev@gmail.com>
2024-02-12 11:14:44 -0300

    refactor: use JS

--- pages/api/v1/status/index.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default function status(req: NextApiRequest, res: NextApiResponse) {
-  return res.status(200).json({ status: "ok" });
-}
