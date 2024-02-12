a94f949a663561e3e396d03a79a471784df5af5f
Diogo <diogo.lara.dev@gmail.com>
2024-02-12 12:31:15 -0300

    refactor: additional settings for SSL
    additional settings for SSL

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,16 @@
 import { Client } from "pg";
 
 const query = async (queryObject) => {
+  const development = process.env.NODE_ENV === "development";
+  const useSSL = process.env.USE_SSL === "true";
+
   const client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: process.env.NODE_ENV === "development" ? false : true
+    ssl: !development && useSSL ? true : false
   });
 
   try {
