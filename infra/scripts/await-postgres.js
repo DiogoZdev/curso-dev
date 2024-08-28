bce26aae03730e30ec9c5e2ffac4dee575853244
Diogo Lara <diogo.lara.dev@gmail.com>
2024-08-28 08:09:36 -0300

    wip: postgres availability test

---
+++ infra/scripts/await-postgres.js
@@ -0,0 +1,21 @@
+const { exec } = require("node:child_process")
+
+async function checkPostgres() {
+  await exec("docker exec postgres-container pg_isready --host localhost", handleResponse);
+
+  function handleResponse(error, stdout) {
+
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+
+      return checkPostgres();
+    }
+
+    console.log("\n🟢  Postgress disponível");
+  }
+}
+
+console.log("🟡  Aguardando disponibilidade do Postgres");
+checkPostgres();
+
+
