f7311419eec8b19f6a82a1229a359f4b65c092d6
Diogo Lara <diogo.lara.dev@gmail.com>
2024-08-28 08:09:06 -0300

    refactor query connection

--- infra/database.js
+++ infra/database.js
@@ -1,30 +1,44 @@
 import { Client } from "pg";
 
-const query = async (queryObject) => {
+async function query (queryObject) {
   let client;
   try {
     client = await getNewClient();
     const res = await client.query(queryObject);
 
     return res;
-  } catch (err) {
+  }
+  catch (err) {
     console.error(err);
-  } finally {
+    throw err;
+  }
+  finally {
     await client.end();
   }
 };
 
-async function getNewClient() {
+function getSSLValues() {
   const production = process.env.NODE_ENV === "production";
   const useSSL = process.env.USE_SSL === "true";
+  const certificate = process.env.POSTGRES_CA;
 
+  if (certificate) {
+    return {
+      ca: certificate,
+    };
+  }
+
+  return production && useSSL ? true : false;
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    ssl: production && useSSL ? true : false
+    ssl: getSSLValues()
   });
 
   await client.connect();
