907ec9b6a8d716b6fc0b423abca5500ab1e2796f
Diogo Lara <diogo.lara.dev@gmail.com>
2024-09-07 10:59:32 -0300

    refactor scripts

--- infra/scripts/await-postgres.js
+++ infra/scripts/await-postgres.js
@@ -3,7 +3,7 @@ const { exec } = require("node:child_process")
 async function checkPostgres() {
   await exec("docker exec postgres-container pg_isready --host localhost", handleResponse);
 
-  function handleResponse(error, stdout) {
+  function handleResponse(error, stdout, stderr) {
 
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
