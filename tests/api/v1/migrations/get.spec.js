3d49e684c523947131b836f6c84b3afebd948dc7
Diogo Lara <diogo.lara.dev@gmail.com>
2024-10-13 16:01:10 -0300

    test: refactor tests

--- tests/api/v1/migrations/get.spec.js
+++ tests/api/v1/migrations/get.spec.js
@@ -1,19 +1,20 @@
-import database from "infra/database";
 import orchestrator from "tests/orchestrator";
 
 beforeAll(async () => {
   await orchestrator.awaitServices();
-  await database.query("DROP schema public cascade; CREATE SCHEMA public;");
+  await orchestrator.clearDatabase();
 });
 
-describe("GET to /api/migrations", () => {
-  test("GET to /api/migrations should return 200", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/migrations", () => {
+  describe("anonymous", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-    const body = await response.json();
+      const body = await response.json();
 
-    expect(Array.isArray(body)).toBe(true);
-    expect(body.length).toBeGreaterThan(0);
-    expect(response.status).toBe(body.length ? 201 : 200);
-  });
+      expect(Array.isArray(body)).toBe(true);
+      expect(body.length).toBeGreaterThan(0);
+      expect(response.status).toBe(body.length ? 201 : 200);
+    });
+  })
 });
