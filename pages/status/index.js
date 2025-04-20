4252536c1e6f86448e8398378927ee0cc63e1290
Diogo Lara <diogo.lara.dev@gmail.com>
2025-04-20 16:37:35 -0300

    feat: ui for status page

--- pages/status/index.js
+++ pages/status/index.js
@@ -3,9 +3,10 @@ import {
   QueryClientProvider,
   useQuery
 } from "@tanstack/react-query";
+import { Card } from "components/card";
 
 async function fetchStatus() {
-  const res = await fetch("http://localhost:3000/api/v1/status");
+  const res = await fetch("/api/v1/status");
   const jsonReponse = await res.json();
   return jsonReponse;
 }
@@ -13,17 +14,70 @@ async function fetchStatus() {
 function StatusPage() {
   const { data } = useQuery({
     queryKey: ["status"],
-    queryFn: fetchStatus
+    queryFn: fetchStatus,
+    refetchInterval: 5000,
   });
 
   return (
-    <div>
-      <p>Updated At: {data?.updated_at}</p>
-      <p>Environment: {data?.environment}</p>
-      <p>Postgres Version: {data?.dependencies?.database?.postgres_version}</p>
-      <p>Used Connections: {data?.dependencies?.database?.used_connections}</p>
-      <p>Max Connections: {data?.dependencies?.database?.max_connections}</p>
-    </div>
+    <>
+      <style>{`
+        * {
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: "Poppins", sans-serif;
+        }
+
+        .page {
+          background-color: #f5f5f5;
+          padding: 2rem;
+        }
+
+        .cards_area {
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+          gap: 2rem;
+          padding: 2rem 0;
+        }
+      `}</style>
+
+      <div className="page">
+
+        <h1>System Status</h1>
+
+        <div className="cards_area">
+          <Card
+            isOk={!!data?.updated_at}
+            title="Updated At"
+            text={new Date(data?.updated_at).toLocaleString("pt-BR")}
+          />
+
+          <Card
+            isOk={!!data?.environment}
+            title="Environment"
+            text={data?.environment}
+          />
+
+          <Card
+            isOk={!!data?.dependencies?.database?.postgres_version}
+            title="Postgres Version"
+            text={data?.dependencies?.database?.postgres_version}
+          />
+
+          <Card
+            isOk={!!data?.dependencies?.database?.used_connections}
+            title="Used Connections"
+            text={data?.dependencies?.database?.used_connections}
+          />
+
+          <Card
+            isOk={!!data?.dependencies?.database?.max_connections}
+            title="Max Connections"
+            text={data?.dependencies?.database?.max_connections}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
