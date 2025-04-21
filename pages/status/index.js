916809362c6324dd2bc6eb24f4c572abd9f8fb3b
Diogo Lara <diogo.lara.dev@gmail.com>
2025-04-21 12:09:31 -0300

    fix: css hydration error

--- pages/status/index.js
+++ pages/status/index.js
@@ -25,7 +25,7 @@ function StatusPage() {
           margin: 0;
           padding: 0;
           box-sizing: border-box;
-          font-family: "Poppins", sans-serif;
+          font-family: sans-serif;
         }
 
         .page {
