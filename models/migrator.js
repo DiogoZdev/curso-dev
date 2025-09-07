f7a6dde8649bdec00f0129c6671be23556e1a80d
Diogo Lara <diogo.lara.dev@gmail.com>
2025-09-07 19:35:26 -0300

    refactor: implement migrator responsibilities

---
+++ models/migrator.js
@@ -0,0 +1,43 @@
+import migrationRunner from "node-pg-migrate";
+import { resolve } from "node:path";
+import database from "infra/database.js";
+
+const migrationOptions = {
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations"
+};
+
+async function execute(config) {
+  let dbClient;
+  const dryRun = config.dryRun;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      ...migrationOptions,
+      dryRun,
+      dbClient,
+    });
+
+    return pendingMigrations;
+  } finally {
+    await dbClient?.end()
+  }
+}
+
+async function listPendingMigrations() {
+  return execute({ dryRun: true });
+}
+
+async function runPendingMigrations() {
+  return execute({ dryRun: false });
+}
+
+export const migrator = {
+  listPendingMigrations,
+  runPendingMigrations
+}
+
